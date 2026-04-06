2a00a4d41b1ed651a379e955d243f83b6874f062
venkatesh-vis <venkateshvishwanadula257@gmail.com>
2026-04-06 13:43:28 +0530

    :rocket: added frontend deployed url

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ const routes = require("./routes");
 connectToDB();
 
 app.use(cors({
-    origin: "http://localhost:8000",
+    origin: "https://uber-three-pi.vercel.app/",
     credentials: true
 }));
 app.use(express.json());
